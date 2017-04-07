021dd6ac062f92764904f50fdeba7c5f5cf83109
Robin Buitendijk <robinbuit@gmail.com>
2017-04-07 15:32:30 +0200

    mVector == and + operators

--- src/mVector.h
+++ src/mVector.h
@@ -21,32 +21,102 @@ template<typename T>
 class mVector{
 
     public:
-        mVector() : length(0){};
-        mVector(int length) : length(length), data(new T[length]){};
+        //CONSTRUCTORS AND DESTRUCTORS
+            //Default constructor
+            mVector() : length(0){};
+
+            //Empty constructor
+            mVector(int length) : length(length), data(new T[length]){};
 
 
+            //initializer_list contructor
+            mVector(initializer_list<T> list) : mVector((int)list.size()) {
+                    typename initializer_list<T>::iterator it;
 
-        mVector(initializer_list<T> list) : mVector((int)list.size()) {
-                typename initializer_list<T>::iterator it;
+                    int i = 0;
+                    for (it = list.begin();it != list.end(); it++)
+                    {
 
-                int i = 0;
-                for (it = list.begin();it != list.end(); it++)
+                        data[i++] = *it;
+                    }
+
+            }
+
+            //Copy constructor
+            mVector(const mVector & other) : mVector(other.size()){
+                for(int i=0; i<length; i++)
                 {
+                    data[i] = other.get(i);
+                }
+            }
 
-                    data[i++] = *it;
+            //Destructor
+            ~mVector(){length = 0; delete [] data;};
+
+        //OPERATORS
+            //If vectors do not contain the same datatype return false
+            template<typename U>
+            bool operator ==(const mVector<U> & other) const{
+                return false;
+            }
+
+            //Check if contend is the same
+            bool operator ==(const mVector<T> & other) const{
+                if (length != other.size())
+                    return false;
+
+                for (int i=0; i<length; i++){
+                    if (data[i] != other.get(i))
+                    return false;
                 }
 
-        }
-        ~mVector(){length = 0; delete [] data;};
+                return true;
+            }
 
-        string toString(){
-            string temp = "mVector[length: "+to_string(length)+", <";
-            for(int i=0; i<length-1; i++){
-                temp += to_string(data[i])+", ";
-            } temp += to_string(data[length-1]);
+            template<typename U>
+            mVector<U> operator+ (const mVector<U> & other) const {
+                if (length != other.size()){    //Size missmatch
+                    mVector<U> ret(0);
+                    return ret;
+                }
 
-            return temp+">]";
-        }
+                mVector<U> ret(other);
+                for(int i=0; i<length; i++){
+                    ret.set(i, ret.get(i)+data[i]);
+                }
+
+
+                return ret;
+            }
+
+
+
+        //FUNCTIONS
+            int size() const{
+                return length;
+            }
+
+            T get(int index) const {
+                return data[index];
+            }
+
+            void set(int index, T value){
+                data[index] = value;
+            }
+
+            //Prints a string reprensentation of this object
+            string toString() const{
+                if (length == 0)
+                    return "mVector[length: 0]";
+                string temp = "mVector[length: "+to_string(length)+", <";
+                for(int i=0; i<length-1; i++){
+                    temp += to_string(data[i])+", ";
+                } temp += to_string(data[length-1]);
+
+                return temp+">]";
+            }
+
+
     private:
         int length;
         T* data;
--- src/main.cxx
+++ src/main.cxx
@@ -8,8 +8,15 @@ int test(){
 
     mVector<double> v(5);
     mVector<int> i = {1,2,3,4,5,6,7};
+    mVector<int> i2 = {1,2,3,4,5,6,7};
     cout << v.toString() << endl;
     cout << i.toString() << endl;
+
+    cout << "v == i: " << (v == i) << endl;
+    cout << "i == i2: " << (i == i2) << endl;
+    cout << (i+i2).toString() << endl;
+    cout << (i+v).toString() << endl;
+
 
     return 0;
 }
