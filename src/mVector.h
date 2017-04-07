03259c2630ddfe77207c8b148d2313b3a3b5cafe
Robin Buitendijk <robinbuit@gmail.com>
2017-04-07 14:59:54 +0200

    merge conflict

--- src/mVector.h
+++ src/mVector.h
@@ -17,17 +17,38 @@ string to_string( const T& n )
 }
 
 
-
+template<typename T>
 class mVector{
 
     public:
         mVector() : length(0){};
-        mVector(int length) : length(length){};
+        mVector(int length) : length(length), data(new T[length]){};
+
+
+
+        mVector(initializer_list<T> list) : mVector((int)list.size()) {
+                typename initializer_list<T>::iterator it;
+
+                int i = 0;
+                for (it = list.begin();it != list.end(); it++)
+                {
+
+                    data[i++] = *it;
+                }
+
+        }
+        ~mVector(){length = 0; delete [] data;};
 
         string toString(){
-            return "mVector[length: "+to_string(length)+",]";
+            string temp = "mVector[length: "+to_string(length)+", <";
+            for(int i=0; i<length-1; i++){
+                temp += to_string(data[i])+", ";
+            } temp += to_string(data[length-1]);
+
+            return temp+">]";
         }
     private:
         int length;
+        T* data;
 
 };
--- src/main.cxx
+++ src/main.cxx
@@ -6,11 +6,15 @@ using namespace std;
 int test(){
     cout << "test lives" << endl;
 
-    mVector v(5);
+    mVector<double> v(5);
+    mVector<int> i = {1,2,3,4,5,6,7};
     cout << v.toString() << endl;
+    cout << i.toString() << endl;
 
     return 0;
 }
+
+
 
 int main()
 {
