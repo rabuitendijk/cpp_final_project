70cfea0b63c9c32a35f2eea40147afe5a25b41b1
lingtjien@hotmail.com <lingtjien@hotmail.com>
2017-05-04 14:34:33 +0200

    matrix class fixed with the matrix vector multiplication

---
+++ src/mMath.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "mMatrix.h"
+#include "mVector.h"
+
+template<typename T>
+mVector<T> operator*( mMatrix<T> m , const mVector<T>& v )
+{
+ if( m.cols() != (unsigned int)v.size() )
+ {
+  std::cout << "matrix and vectors have different sizes and can not be multiplied" << std::endl;
+  return mVector<T>( v.size() );
+ }
+ 
+ mVector<T> result( v.size() );
+ for( unsigned int i=0; i<m.rows(); i++ )
+ {
+  for( unsigned int j=0; j<m.cols(); j++ )
+  {
+   result[i] += v[j] * m[{i,j}];
+  }
+ }
+ 
+ return result;
+}
--- src/mMatrix.h
+++ src/mMatrix.h
@@ -8,40 +8,67 @@ template<typename T>
 class mMatrix
 {
 private:
+<<<<<<< HEAD
+ const unsigned int n_rows;
+ const unsigned int n_cols;
+ 
+=======
  const unsigned int rows;
  const unsigned int cols;
 
+>>>>>>> d9e2879b9d40e8cbb313543c01ceaf9842e540f0
  std::map<unsigned int,T> map; //all data is stored in a map with the index and the value
 
  enum class Type
  {
   row,
   col
+<<<<<<< HEAD
+ }; 
+ 
+ Type type;
+ 
+public: 
+ mMatrix( const unsigned int& rows , const unsigned int& cols , const Type& type = Type::row ) : n_rows( rows ) , n_cols( cols ) , type( type ) {}  //default is row major, thus all rows will be set behind each other in a continuous data block
+
+ unsigned int cols() const { return this->n_cols; }
+=======
  } type;
 
 public:
  mMatrix( const unsigned int& rows , const unsigned int& cols , const Type& type = Type::row ) : rows( rows ) , cols( cols ) , type( type ) {}  //default is row major, thus all rows will be set behind each other in a continuous data block
+>>>>>>> d9e2879b9d40e8cbb313543c01ceaf9842e540f0
 
+ unsigned int rows() const { return this->n_rows; }
+ 
  unsigned int size() const
  {
-  return this->rows*this->cols;
+  return this->n_rows*this->n_cols;
  }
 
 //  void insert( const unsigned int& row , const unsigned int& col , const T& value )
 //  {
-//   if( row > this->rows || col > this->cols )
+//   if( row > this->rows() || col > this->cols() )
 //   {
-//    std::cout << "the value to be added is out of bound, matrix is " << this->rows << "x" << this->cols << " but value is (" << row << "," << col << ")" << std::endl;
+//    std::cout << "the value to be added is out of bound, matrix is " << this->rows() << "x" << this->cols() << " but value is (" << row << "," << col << ")" << std::endl;
 //    return;
 //   }
 //
 //   switch( this->type )
 //   {
+<<<<<<< HEAD
+//    case Type::row : 
+//     this->map.insert( std::pair<unsigned int,T>( col*this->rows()+row , value ) );
+//    break;
+//    case Type::col :
+//     this->map.insert( std::pair<unsigned int,T>( row*this->cols()+col , value ) );  
+=======
 //    case Type::row :
 //     this->map.insert( std::pair<unsigned int,T>( row*this->rows+col , value ) );
 //    break;
 //    case Type::col :
 //     this->map.insert( std::pair<unsigned int,T>( col*this->cols+row , value ) );
+>>>>>>> d9e2879b9d40e8cbb313543c01ceaf9842e540f0
 //    break;
 //   }
 //
@@ -50,19 +77,27 @@ public:
 
  T& operator[] ( const std::array<unsigned int,2>& array )
  {
-  if( array[0] > this->rows || array[1] > this->cols )
+  if( array[0] >= this->rows() || array[1] >= this->cols() )
   {
-   std::cout << "the value to be added is out of bound, matrix is " << this->rows << "x" << this->cols << " but value is (" << array[0] << "," << array[1] << ")" << std::endl;
+   std::cout << "the value to be added is out of bound, matrix is " << this->n_rows << "x" << this->n_cols << " but value is (" << array[0] << "," << array[1] << ")" << std::endl;
   }
 
   unsigned int index;
   switch( this->type )
   {
+<<<<<<< HEAD
+   case Type::row : 
+    index = array[1]*this->rows()+array[0];
+   break;
+   case Type::col :
+    index = array[0]*this->cols()+array[1]; 
+=======
    case Type::row :
     index = array[0]*this->rows+array[1];
    break;
    case Type::col :
     index = array[1]*this->cols+array[0];
+>>>>>>> d9e2879b9d40e8cbb313543c01ceaf9842e540f0
    break;
   }
 
@@ -73,22 +108,32 @@ public:
  {
   std::ostringstream string;
   typename std::map<unsigned int,T>::iterator it;
+<<<<<<< HEAD
+  
+  for( std::size_t i=0; i<this->rows(); i++ )
+=======
 
   for( std::size_t i=0; i<this->cols; i++ )
+>>>>>>> d9e2879b9d40e8cbb313543c01ceaf9842e540f0
   {
-   for( std::size_t j=0; j<this->rows; j++ )
+   for( std::size_t j=0; j<this->cols(); j++ )
    {
     switch( this->type )
     {
+<<<<<<< HEAD
+     case Type::row : 
+      it = this->map.find( j*this->rows()+i );
+=======
      case Type::row :
       it = this->map.find( i*this->rows+j );
+>>>>>>> d9e2879b9d40e8cbb313543c01ceaf9842e540f0
       if( it != this->map.end() )
        string << it->second << " ";
       else
        string << "0 ";
      break;
      case Type::col :
-      it = this->map.find( i+this->cols*j );
+      it = this->map.find( i+this->cols()*j );
       if( it != this->map.end() )
        string << it->second << " ";
       else
