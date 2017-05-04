bcb49a70ea4657df2170aad4727ca7502ad0fa3d
lingtjien@hotmail.com <lingtjien@hotmail.com>
2017-05-04 15:10:39 +0200

    add ling test

---
+++ src/ling_test.cxx
@@ -0,0 +1,30 @@
+#include "mMath.h"
+
+int main()
+{
+ unsigned int row = 2;
+ unsigned int col = 3;
+ 
+ mMatrix<double> m(row,col);
+ mVector<double> v(col);
+ 
+ for( unsigned int i=0; i<col; i++ )
+ {
+  v[i] = i;
+  for( unsigned int j=0; j<row; j++ )
+  {
+   m[{j,i}] = i+j;
+  }
+ }
+
+ mVector<double> u = m*v;
+ 
+ v.print();
+ u.print();
+ 
+ std::cout << std::endl;
+ 
+ m.print();
+ 
+ return 0;
+}
